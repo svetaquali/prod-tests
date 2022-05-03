f3306c3bebf9dd5e46f563ca2cd805664f389f16
David B <96681520+davidbquali@users.noreply.github.com>
2022-05-04 00:10:04 +0300

    Update main.tf

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -51,6 +51,26 @@ resource "aws_db_subnet_group" "rds" {
   }
 }
 
+resource "aws_security_group" "rds" {
+  name        = "rds-${var.sandbox_id}_sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_default_vpc.default.id}"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 resource "aws_db_instance" "default" {
   allocated_storage    = var.allocated_storage
