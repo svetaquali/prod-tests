1fd37ee0b0739708277e98d30d7fe398eab4e8a9
Johnathan Viduchinsky <Johnathan.V@quali.com>
2022-05-03 13:55:36 +0300

    adds terraform modules - rds and s3

---
+++ terraform/rds/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">3.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_vpc" "default" {
+  default = true
+} 
+
+locals {
+  sizeMap = {
+    "small" = "db.t2.small"
+    "medium" = "db.t2.medium"
+    "large" = "db.t2.large"
+  }
+  instance_class = lookup(local.sizeMap, lower(var.size), "db.t4g.medium")
+  # sandbox_id = "sb${substr( uuid() , 0 ,6)}"
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+data "aws_subnet_ids" "apps_subnets" {
+  vpc_id = "${data.aws_vpc.default.id}"
+  filter {
+    name = "tag:Name"
+    values = ["app-subnet*"]
+  }
+}
+
+resource "aws_db_subnet_group" "rds" {
+  name = "rds-${var.sandbox_id}-subnet-group"
+  subnet_ids = data.aws_subnet_ids.apps_subnets.ids
+
+  tags = {
+    Name = "RDS-subnet-group"
+  }
+}
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = var.allocated_storage
+  storage_type         = var.storage_type
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = local.instance_class
+  identifier           = "rds-${var.sandbox_id}"
+  name                 = "${var.db_name}"
+  username             = "${var.username}"
+  password             = "${random_password.password.result}"
+  publicly_accessible  = true
+  db_subnet_group_name = "${aws_db_subnet_group.rds.id}"
+  vpc_security_group_ids    = ["${aws_security_group.rds.id}"]
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "Ignore"
+}
