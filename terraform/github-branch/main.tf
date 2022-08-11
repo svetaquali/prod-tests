dc2bff7476b6004fd90a1fc657fe28ebefa1065b
johnathanvidu <johnathan.v@qualisystems.com>
2022-08-11 23:58:27 +0300

    Create main.tf

---
+++ terraform/github-branch/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.token
+}
+
+resource "github_branch" "development" {
+  repository = var.repo
+  branch     = var.branch
+}
