25006d4aed4e949c951de1900fca650f3c483134
johnathanvidu <johnathan.v@qualisystems.com>
2023-01-04 15:04:11 +0200

    Create variables.tf

---
+++ terraform/azure_vm/variables.tf
@@ -0,0 +1,31 @@
+variable "tenant_id" {
+  type = string
+  description = "Tenant id"
+}
+
+variable "subscription_id" {
+  type = string
+  description = "Subscription id"
+}
+
+variable "client_id" {
+  type = string
+  description = "Client id"
+}
+
+variable "client_secret" {
+  type = string
+  description = "Client Secret"
+}
+
+variable "vm_name" {
+  type = string
+  default = "MyVM"
+  description = "VM Name"
+}
+
+variable "resource_group" {
+  type = string
+  default = "VidoRG"
+  description = "resource group name"
+}
