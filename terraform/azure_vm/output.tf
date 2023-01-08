e2bd6ccb78df817be10754f5cca7c116b5854900
johnathanvidu <johnathan.v@qualisystems.com>
2023-01-08 23:26:25 +0200

    Update output.tf

--- terraform/azure_vm/output.tf
+++ terraform/azure_vm/output.tf
@@ -1,3 +1,7 @@
 output "vm_id" {
   value = azurerm_windows_virtual_machine.example.id
 }
+
+output "vm_power_state" {
+  value = jsondecode(azurerm_virtual_machine_extension.example.settings)["output"]
+}
