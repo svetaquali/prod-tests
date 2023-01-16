11206473bbbd4d04043364f1f5b3dd5e21d08c15
johnathanvidu <johnathan.v@qualisystems.com>
2023-01-16 21:58:52 +0200

    Update main.tf

--- terraform/azure_vm_legacy/main.tf
+++ terraform/azure_vm_legacy/main.tf
@@ -72,7 +72,7 @@ resource "azurerm_virtual_machine" "example" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "19_04-gen2"
+    sku       = "19_04"
     version   = "latest"
   }
   storage_os_disk {
