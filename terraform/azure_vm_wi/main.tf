e2bc053c25ef4a7659e100812c9dd2475af800dd
Sveta C <svetlana.c@quali.com>
2023-03-23 13:34:56 +0200

    use public_ip_alloc_method

--- terraform/azure_vm_wi/main.tf
+++ terraform/azure_vm_wi/main.tf
@@ -48,7 +48,7 @@ resource "azurerm_public_ip" "example" {
   name                = "example-public-ip"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
+  allocation_method   = var.public_ip_alloc_method
 }
 
 ## <https://www.terraform.io/docs/providers/azurerm/r/network_interface.html>
