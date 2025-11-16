28e2dc9a7b71ed48d8970af4abd403f294a87b6e
svetaquali <svetlana.c@quali.com>
2025-11-16 19:48:20 +0200

    add printenv

--- terraform/azure_rg/main.tf
+++ terraform/azure_rg/main.tf
@@ -15,3 +15,9 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = "westus2"
 }
+
+resource "null_resource" "print_env" {
+  provisioner "local-exec" {
+    command = "printenv"
+  }
+}
